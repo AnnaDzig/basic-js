f9a1f30eeaa5f95a23b0416e9b70a45812d463e9
AnnaDzig <adzyhota@protonmail.com>
2025-11-10 22:32:51 +0100

    feat: implement transform to handle control sequences and produce transformed array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,49 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const res = [];
+  const DISCARD_NEXT = '--discard-next';
+  const DISCARD_PREV = '--discard-prev';
+  const DOUBLE_NEXT = '--double-next';
+  const DOUBLE_PREV = '--double-prev';
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+
+    switch (current) {
+      case DISCARD_NEXT:
+        i++;                
+        break;
+
+      case DISCARD_PREV:
+        if (res.length && arr[i - 2] !== DISCARD_NEXT) {
+          res.pop();       
+        }
+        break;
+
+      case DOUBLE_NEXT:
+        if (i + 1 < arr.length) {
+          res.push(arr[i + 1]);
+        }
+        break;
+
+      case DOUBLE_PREV:
+        if (i > 0 && res.length && arr[i - 2] !== DISCARD_NEXT) {
+          res.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        res.push(current);
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
