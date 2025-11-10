3a4b3b5660040dcf759172796e78f33feb773345
AnnaDzig <adzyhota@protonmail.com>
2025-11-10 22:25:52 +0100

    feat: implement calculateHanoi, isMAC48Address, getMatrixElementsSum, minesweeper func

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -23,9 +23,42 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  const res = Array.from({ length: rows }, () => Array(cols).fill(0));
+
+  const dirs = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [ 0, -1],          [ 0, 1],
+    [ 1, -1], [ 1, 0], [ 1, 1]
+  ];
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      let count = 0;
+
+      for (const [dr, dc] of dirs) {
+        const nr = r + dr;
+        const nc = c + dc;
+
+        if (
+          nr >= 0 &&
+          nr < rows &&
+          nc >= 0 &&
+          nc < cols &&
+          matrix[nr][nc] === true
+        ) {
+          count++;
+        }
+      }
+
+      res[r][c] = count;
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
